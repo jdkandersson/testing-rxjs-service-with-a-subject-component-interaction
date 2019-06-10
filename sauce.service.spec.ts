50a129c4ed633b10a8679834f89409ea98b0c78e
jdkandersson <51036209+jdkandersson@users.noreply.github.com>
2019-06-10 12:56:37 +1000

    Create sauce.service.spec.ts

---
+++ sauce.service.spec.ts
@@ -0,0 +1,40 @@
+// sauce.service.spec.ts
+import { cold } from 'jasmine-marbles';
+
+import { SauceService } from './sauce.service';
+
+
+describe('SauceService', () => {
+  let service: SauceService;
+
+  beforeEach(() => {
+    service = new SauceService();
+  });
+
+  describe('construction', () => {
+    it('should emit initial sauce', () => {
+      // Setting up expected observable
+      const expectedMarbles = 'a';
+      const expected = cold(expectedMarbles, {a: 'initial sauce'});
+
+      // Checking sauce$
+      expect(service.sauce$()).toBeObservable(expected);
+    });
+  });
+
+  describe('setSauce', () => {
+    it('should emit new sauce', () => {
+      // Defining marbles
+      const setSauceCallMarbles = '-a';
+      const expectedMarbles =     'bc';
+
+      // Setting up setSauce call
+      cold(setSauceCallMarbles, {a: 'new sauce'})
+        .subscribe(sauce => {service.setSauce(sauce)});
+
+      // Checking sauce$
+      const expected = cold(expectedMarbles, {b: 'initial sauce', c: 'new sauce'});
+      expect(service.sauce$()).toBeObservable(expected);
+    });
+  });
+});
